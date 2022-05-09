50b1524e42d60ae73b105c2f551075da6231eb8d
Flex-Bot-Hosting <104250526+Flex-Bot-Hosting@users.noreply.github.com>
2022-05-09 16:31:11 +1000

    Create index.php

---
+++ index.php
@@ -0,0 +1,51 @@
+<table>
+<tbody><tr>
+    <th>Name</th>
+    <th>Searches</th>
+    <th>NameMC Link</th>
+    <th>Availability</th>
+</tr>
+<tr>
+<td>
+    (php api code name)
+</td>
+<td>
+    (php api code searches)
+</td>
+<td>
+    (php api code link)
+</td>
+<td>
+    (php api code available)
+</td>
+</tr>
+<tr>
+<td>
+    (php api code name)
+</td>
+<td>
+    (php api code searches)
+</td>
+<td>
+    (php api code link)
+</td>
+<td>
+    (php api code available)
+</td>
+</tr>
+<tr>
+<td>
+    (php api code name)
+</td>
+<td>
+    (php api code searches)
+</td>
+<td>
+    (php api code link)
+</td>
+<td>
+    (php api code available)
+</td>
+</tr>
+
+</tbody></table>
