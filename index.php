3fa8aea9c30e73fce0005ac17d4be7838f531fab
Flex-Bot-Hosting <104250526+Flex-Bot-Hosting@users.noreply.github.com>
2022-05-09 16:39:59 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,51 +1,60 @@
-<table>
-<tbody><tr>
-    <th>Name</th>
-    <th>Searches</th>
-    <th>NameMC Link</th>
-    <th>Availability</th>
-</tr>
-<tr>
-<td>
-    (php api code name)
-</td>
-<td>
-    (php api code searches)
-</td>
-<td>
-    (php api code link)
-</td>
-<td>
-    (php api code available)
-</td>
-</tr>
-<tr>
-<td>
-    (php api code name)
-</td>
-<td>
-    (php api code searches)
-</td>
-<td>
-    (php api code link)
-</td>
-<td>
-    (php api code available)
-</td>
-</tr>
-<tr>
-<td>
-    (php api code name)
-</td>
-<td>
-    (php api code searches)
-</td>
-<td>
-    (php api code link)
-</td>
-<td>
-    (php api code available)
-</td>
-</tr>
+<html>
+<head>
+    <link rel="stylesheet" href="/css/main.css" />
+    <title>Neptune Sniper</title>
+</head>
+<body>
+<h1>Neptune Sniper</h1>
+    <table>
+    <tbody><tr>
+        <th>Name</th>
+        <th>Searches</th>
+        <th>NameMC Link</th>
+        <th>Availability</th>
+    </tr>
+    <tr>
+    <td>
+        (php api code name)
+    </td>
+    <td>
+        (php api code searches)
+    </td>
+    <td>
+        (php api code link)
+    </td>
+    <td>
+        (php api code available)
+    </td>
+    </tr>
+    <tr>
+    <td>
+        (php api code name)
+    </td>
+    <td>
+        (php api code searches)
+    </td>
+    <td>
+        (php api code link)
+    </td>
+    <td>
+        (php api code available)
+    </td>
+    </tr>
+    <tr>
+    <td>
+        (php api code name)
+    </td>
+    <td>
+        (php api code searches)
+    </td>
+    <td>
+        (php api code link)
+    </td>
+    <td>
+        (php api code available)
+    </td>
+    </tr>
 
-</tbody></table>
+    </tbody></table>
+</body>
+</html>
