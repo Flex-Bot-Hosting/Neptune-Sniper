9a09a576bb37c129c071f31ff8ee1421a2ed48ed
Rodney_RWR <85048343+Rodney-RWR@users.noreply.github.com>
2022-05-24 17:35:48 +1000

    Update index.php

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ if(isset($_GET['searchquery'])) {
         // Display data
         echo "<h2>$username</h2> <br>";
         echo "<h3>UUID: $uuid</h3> <br>";
-        echo "<h3>Skin: <applet code='net.minecraft.skintest.ModelPreviewApplet' archive='http://www.minecraft.net/skin/skintest.jar' codebase='.' width='320' height='320'> <param name='name' value='535' /> </applet></h3>";
+        echo "<h3>Skin: http://textures.minecraft.net/texture/$uuid</h3>";
         echo "<h3><a href='https://namemc.com/$username'>NameMC Link</a></h3>";
     }
 }
