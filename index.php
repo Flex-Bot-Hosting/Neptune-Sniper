001d73a3f9810430f55fab6a62109af814ed7420
Rodney_RWR <85048343+Rodney-RWR@users.noreply.github.com>
2022-05-25 14:49:43 +1000

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <body>
 <h1>Neptune Sniper</h1>
   <form action="/" method="get">
-      <input type="text" placeholder="Username or UUID" name="searchquery">
+      <input type="search" placeholder="Username or UUID" name="searchquery">
   </form>
 <?php
 if(isset($_GET['searchquery'])) {
