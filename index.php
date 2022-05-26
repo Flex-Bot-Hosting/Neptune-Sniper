278a44a9786fbfb4d2b16726b221fa8cfce02ee5
Rodney_RWR <85048343+Rodney-RWR@users.noreply.github.com>
2022-05-27 09:27:55 +1000

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ if(isset($_GET['searchquery'])) {
         }
 
     // Read the JSON file
-    if(strlen($search) > 16) {
+    if(strlen($var0) > 16) {
         $json = file_get_contents("https://api.mojang.com/user/profile/$var0");
   } else {
         $json = file_get_contents("https://api.mojang.com/users/profiles/minecraft/$var0");
@@ -40,7 +40,7 @@ if(isset($_GET['searchquery'])) {
         echo "<h3><a href='https://namemc.com/$username'>NameMC Link</a></h3>";
     }
         if(isset($var1)) {
-            if(strlen($search) > 16) {
+            if(strlen($var1) > 16) {
                 $json1 = file_get_contents("https://api.mojang.com/user/profile/$var1");
           } else {
                 $json1 = file_get_contents("https://api.mojang.com/users/profiles/minecraft/$var1");
@@ -63,7 +63,7 @@ if(isset($_GET['searchquery'])) {
             }
         }
             if(isset($var2)) {
-            if(strlen($search) > 16) {
+            if(strlen($var2) > 16) {
                 $json2 = file_get_contents("https://api.mojang.com/user/profile/$var2");
           } else {
                 $json2 = file_get_contents("https://api.mojang.com/users/profiles/minecraft/$var2");
@@ -85,6 +85,29 @@ if(isset($_GET['searchquery'])) {
                 echo "<h3><a href='https://namemc.com/$username2'>NameMC Link</a></h3>";
             }
         }
+                if(isset($var3)) {
+                    if(strlen($var3) > 16) {
+                        $json3 = file_get_contents("https://api.mojang.com/user/profile/$var3");
+                  } else {
+                        $json3 = file_get_contents("https://api.mojang.com/users/profiles/minecraft/$var3");
+                    }
+
+                 // Decode the JSON file
+                    $json_data3 = json_decode($json3,true);
+
+                    if(!isset($json_data2)) {
+                        echo "<h2> User does not exsist </h2>";
+                  } else {
+                        $username1 = $json_data3['name'];
+                        $uuid1 = $json_data3['id'];
+
+                // Display data
+                        echo "<h2>$username3</h2> <br>";
+                        echo "<h3>UUID: $uuid3</h3> <br>";
+                        echo "<h3>Skin: <img src='https://crafatar.com/renders/body/$uuid3' alt=''></h3>";
+                        echo "<h3><a href='https://namemc.com/$username3'>NameMC Link</a></h3>";
+                    }
+                }
 }
 ?>
 </body>
