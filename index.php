1eaad7e747ca70e77e0d9c13af85cf424bce396a
Rodney_RWR <85048343+Rodney-RWR@users.noreply.github.com>
2022-05-26 15:06:43 +1000

    Update index.php

--- index.php
+++ index.php
@@ -12,9 +12,12 @@
 <?php
 if(isset($_GET['searchquery'])) {
     $search = $_GET['searchquery'];
+    foreach (explode(', ', $search) as $key => $value){
+        ${'var'.$key} = $value
+    }
 
-    // Read the JSON file 
-    $json = file_get_contents("https://api.mojang.com/users/profiles/minecraft/$search");
+    // Read the JSON file
+    $json = file_get_contents("https://api.mojang.com/users/profiles/minecraft/$var0");
 
     // Decode the JSON file
     $json_data = json_decode($json,true);
@@ -31,6 +34,25 @@ if(isset($_GET['searchquery'])) {
         echo "<h3>Skin: <img src='https://crafatar.com/renders/body/$uuid' alt=''></h3>";
         echo "<h3><a href='https://namemc.com/$username'>NameMC Link</a></h3>";
     }
+        if(isset($var1)) {
+            $json1 = file_get_contents("https://api.mojang.com/users/profiles/minecraft/$var1");
+
+         // Decode the JSON file
+            $json_data1 = json_decode($json1,true);
+
+            if(!isset($json_data1)) {
+                echo "<h2> User does not exsist </h2>";
+          } else {
+                $username1 = $json_data1['name'];
+                $uuid1 = $json_data1['id'];
+
+        // Display data
+                echo "<h2>$username1</h2> <br>";
+                echo "<h3>UUID: $uuid1</h3> <br>";
+                echo "<h3>Skin: <img src='https://crafatar.com/renders/body/$uuid1' alt=''></h3>";
+                echo "<h3><a href='https://namemc.com/$username1'>NameMC Link</a></h3>";
+            }
+        }
 }
 ?>
 </body>
