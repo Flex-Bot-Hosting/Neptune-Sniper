e5374cc5bd4301cd61aa0cc66cca73197c8bd537
Rodney_RWR <85048343+Rodney-RWR@users.noreply.github.com>
2022-08-03 14:00:40 +1000

    shortened code
    Put the get username code in a for loop so it can theoretically get an infinite amount of names

--- index.php
+++ index.php
@@ -15,130 +15,28 @@ if(isset($_GET['searchquery'])) {
     $search = $_GET['searchquery'];
     foreach (explode(', ', $search) as $key => $value){
         ${'var'.$key} = $value;
+        if(strlen($var.$key) > 16) {
+            $json = file_get_contents("https://api.mojang.com/user/profile/$var.$key");
+        } else {
+            $json = file_get_contents("https://api.mojang.com/users/profiles/minecraft/$var.$key");
         }
 
-    // Read the JSON file
-    if(strlen($var0) > 16) {
-        $json = file_get_contents("https://api.mojang.com/user/profile/$var0");
-  } else {
-        $json = file_get_contents("https://api.mojang.com/users/profiles/minecraft/$var0");
-    }
+        // Decode the JSON file
+        $json_data = json_decode($json,true);
 
-    // Decode the JSON file
-    $json_data = json_decode($json,true);
-
-    if(!isset($json_data)) {
-        echo "<h2> User does not exsist </h2>";
-    } else {
-        $username = $json_data['name'];
-        $uuid = $json_data['id'];
-
-        // Display data
-        echo "<h2>$username</h2> <br>";
-        echo "<h3>UUID: $uuid</h3> <br>";
-        echo "<h3>Skin: <img src='https://crafatar.com/renders/body/$uuid' alt=''></h3>";
-        echo "<h3><a href='https://namemc.com/$username'>NameMC Link</a></h3>";
-    }
-        if(isset($var1)) {
-            if(strlen($var1) > 16) {
-                $json1 = file_get_contents("https://api.mojang.com/user/profile/$var1");
-          } else {
-                $json1 = file_get_contents("https://api.mojang.com/users/profiles/minecraft/$var1");
-            }
-
-         // Decode the JSON file
-            $json_data1 = json_decode($json1,true);
-
-            if(!isset($json_data1)) {
-                echo "<hr>";
-                echo "<h2> User does not exsist </h2>";
-          } else {
-                $username1 = $json_data1['name'];
-                $uuid1 = $json_data1['id'];
-
-        // Display data
-                echo "<hr>";
-                echo "<h2>$username1</h2> <br>";
-                echo "<h3>UUID: $uuid1</h3> <br>";
-                echo "<h3>Skin: <img src='https://crafatar.com/renders/body/$uuid1' alt=''></h3>";
-                echo "<h3><a href='https://namemc.com/$username1'>NameMC Link</a></h3>";
-            }
-        }
-            if(isset($var2)) {
-                if(strlen($var2) > 16) {
-                    $json2 = file_get_contents("https://api.mojang.com/user/profile/$var2");
-              } else {
-                    $json2 = file_get_contents("https://api.mojang.com/users/profiles/minecraft/$var2");
-                }
-
-             // Decode the JSON file
-                $json_data2 = json_decode($json2,true);
-
-                if(!isset($json_data2)) {
-                    echo "<hr>";
-                    echo "<h2> User does not exsist </h2>";
-              } else {
-                    $username2 = $json_data2['name'];
-                    $uuid2 = $json_data2['id'];
+        if(!isset($json_data)) {
+            echo "<h2> User does not exsist </h2>";
+        } else {
+            $username = $json_data['name'];
+            $uuid = $json_data['id'];
 
             // Display data
-                    echo "<hr>";
-                    echo "<h2>$username2</h2> <br>";
-                    echo "<h3>UUID: $uuid2</h3> <br>";
-                    echo "<h3>Skin: <img src='https://crafatar.com/renders/body/$uuid2' alt=''></h3>";
-                    echo "<h3><a href='https://namemc.com/$username2'>NameMC Link</a></h3>";
-                }
-            }
-                if(isset($var3)) {
-                    if(strlen($var3) > 16) {
-                        $json3 = file_get_contents("https://api.mojang.com/user/profile/$var3");
-                  } else {
-                        $json3 = file_get_contents("https://api.mojang.com/users/profiles/minecraft/$var3");
-                    }
-
-                 // Decode the JSON file
-                    $json_data3 = json_decode($json3,true);
-
-                    if(!isset($json_data2)) {
-                        echo "<hr>";
-                        echo "<h2> User does not exsist </h2>";
-                  } else {
-                        $username3 = $json_data3['name'];
-                        $uuid3 = $json_data3['id'];
-
-                // Display data
-                        echo "<hr>";
-                        echo "<h2>$username3</h2> <br>";
-                        echo "<h3>UUID: $uuid3</h3> <br>";
-                        echo "<h3>Skin: <img src='https://crafatar.com/renders/body/$uuid3' alt=''></h3>";
-                        echo "<h3><a href='https://namemc.com/$username3'>NameMC Link</a></h3>";
-                    }
-                }
-                    if(isset($var4)) {
-                        if(strlen($var4) > 16) {
-                            $json4 = file_get_contents("https://api.mojang.com/user/profile/$var4");
-                      } else {
-                            $json4 = file_get_contents("https://api.mojang.com/users/profiles/minecraft/$var4");
-                        }
-
-                     // Decode the JSON file
-                        $json_data4 = json_decode($json4,true);
-
-                        if(!isset($json_data4)) {
-                            echo "<hr>";
-                            echo "<h2> User does not exsist </h2>";
-                      } else {
-                            $username4 = $json_data4['name'];
-                            $uuid4 = $json_data4['id'];
-
-                    // Display data
-                            echo "<hr>";
-                            echo "<h2>$username4</h2> <br>";
-                            echo "<h3>UUID: $uuid4</h3> <br>";
-                            echo "<h3>Skin: <img src='https://crafatar.com/renders/body/$uuid4' alt=''></h3>";
-                            echo "<h3><a href='https://namemc.com/$username4'>NameMC Link</a></h3>";
-                        }
-                    }
+            echo "<h2>$username</h2> <br>";
+            echo "<h3>UUID: $uuid</h3> <br>";
+            echo "<h3>Skin: <img src='https://crafatar.com/renders/body/$uuid' alt=''></h3>";
+            echo "<h3><a href='https://namemc.com/$username'>NameMC Link</a></h3>";
+        }
+    }
 }
 ?>
 </body>
